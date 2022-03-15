b6d586717084d6b8a5a60ce96e7e72df32a024b3
dalred <RcrF63gGG7HpbAT>
2022-03-15 23:43:29 +0300

    final delete path

--- demostration_solution/app.py
+++ demostration_solution/app.py
@@ -31,7 +31,6 @@ def create_data(app, db):
 
 
 app = create_app(Config())
-app.debug = True
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=10001, debug=True)
+    app.run(host="localhost")
